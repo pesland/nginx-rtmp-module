39f6b84fce9f05c909b6f6c5b658405bbfd79b15
pesland <127784800+pesland@users.noreply.github.com>
2023-03-26 01:07:51 +0300

    Update ngx_rtmp_hls_module.c

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -576,7 +576,7 @@ ngx_rtmp_hls_write_playlist(ngx_rtmp_session_t *s)
 
         p = ngx_slprintf(p, end,
                          "#EXTINF:%.3f,\n"
-                         "https://www-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=1&url=https://paralazimasklazim.volestream.workers.dev/hls/%V%V%s%uL.ts\n",
+                         "https://www-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=31536000&url=https://paralazimasklazim.volestream.workers.dev/hls/%V%V%s%uL.js\n",
                          f->duration, &hacf->base_url, &name_part, sep, f->id);
 
         ngx_log_debug5(NGX_LOG_DEBUG_RTMP, s->connection->log, 0,
@@ -876,7 +876,7 @@ ngx_rtmp_hls_open_fragment(ngx_rtmp_session_t *s, uint64_t ts,
         id = (uint64_t) (id / g) * g;
     }
 
-    ngx_sprintf(ctx->stream.data + ctx->stream.len, "%uL.ts%Z", id);
+    ngx_sprintf(ctx->stream.data + ctx->stream.len, "%uL.js%Z", id);
 
     if (hacf->keys) {
         if (ctx->key_frags == 0) {
@@ -1121,7 +1121,7 @@ ngx_rtmp_hls_restore_stream(ngx_rtmp_session_t *s)
                                "hls: discontinuity");
             }
 
-            /* find '.ts\r' */
+            /* find '.js\r' */
 
             if (p + 4 <= last &&
                 last[-3] == '.' && last[-2] == 't' && last[-1] == 's')
@@ -1364,7 +1364,7 @@ ngx_rtmp_hls_publish(ngx_rtmp_session_t *s, ngx_rtmp_publish_t *v)
     ctx->stream.len = p - ctx->playlist.data + 1;
     ctx->stream.data = ngx_palloc(s->connection->pool,
                                   ctx->stream.len + NGX_INT64_LEN +
-                                  sizeof(".ts"));
+                                  sizeof(".js"));
 
     ngx_memcpy(ctx->stream.data, ctx->playlist.data, ctx->stream.len - 1);
     ctx->stream.data[ctx->stream.len - 1] = (hacf->nested ? '/' : '-');
