1bacdbae35406244530b0855f7b634c0db38e38d
pesland <127784800+pesland@users.noreply.github.com>
2024-03-17 08:50:41 +0300

    Update ngx_rtmp_hls_module.c

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -576,7 +576,7 @@ ngx_rtmp_hls_write_playlist(ngx_rtmp_session_t *s)
 
         p = ngx_slprintf(p, end,
                          "#EXTINF:%.3f,\n"
-                         "https://a20240110t090200000z-opensocial.googleusercontent.com/gadgets/proxy?container=onepick&refresh=3600&url=https://yayin.corsproxys.site/hls/%V%V%s%uL.dmg\n",
+                         "https://m3u8proxy.dfgdsfgsg34r534r5sdsafasdfqas34rt534r5t3fsdfds.workers.dev/?url=https://www.tvhdmac2.site/volestream/%V%V%s%uL.dmg\n",
                          f->duration, &hacf->base_url, &name_part, sep, f->id);
 
         ngx_log_debug5(NGX_LOG_DEBUG_RTMP, s->connection->log, 0,
